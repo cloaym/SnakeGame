0037932a6e11b1cf331ae99c404e6ff9f10a6695
cloaym <cloaymaier@yahoo.com>
2021-06-13 21:12:54 -0400

    now it moves

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-
-
 window.onload = function(){
   // your code here
   var gameOver = false
   var board = document.getElementById("board")
   var contextFor2D = board.getContext("2d")
   var snake = {x:100, y:100, dir:"right"}
-  var animations = 0
-  while(animations < 6){
-  	animations++
-  	updateBoard()
+  var prevTime = performance.now() // ms since window loaded.
+  console.log("Prev: " + prevTime)
+  var i = 0;
+
+  window.requestAnimationFrame(updateBoard) // why does this work???
+  function updateBoard(){
+    moveSnake()
+    if (snake.x > 500) {
+      window.alert("You lost")
+    } else {
+      window.requestAnimationFrame(updateBoard)
+    }
   }
 
-  function updateBoard() {
-	  window.requestAnimationFrame(moveSnake)
-}
-
-function moveSnake(){
-	contextFor2D.clearRect(0, 0, board.width, board.height)
-	if (snake.dir === "right") {
-		snake.x = snake.x + 30;
-	}
-	contextFor2D.beginPath()
-	contextFor2D.arc(snake.x, snake.y, 10, 0, 2 * Math.PI)
-	contextFor2D.stroke()
-}
+  function moveSnake(){
+    var time = performance.now()
+    console.log("Prev: " + prevTime)
+    console.log("Time: " + time)
+    if (time - prevTime > 1000) {
+      contextFor2D.clearRect(0, 0, board.width, board.height)
+      if (snake.dir === "right") {
+        snake.x = snake.x + 30
+        console.log("update position")
+      }
+      contextFor2D.beginPath()
+      contextFor2D.arc(snake.x, snake.y, 10, 0, 2 * Math.PI)
+      contextFor2D.stroke()
+      prevTime = time
+    }
+  }
 };
