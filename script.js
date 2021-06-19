fe585d6ca5c656e43df5571f95dfcf9cb8c745f5
cloaym <cloaymaier@yahoo.com>
2021-06-19 19:24:08 -0400

    now we have targets

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 window.onload = function(){
-  // your code here
-  var gameOver = false
+  const nTilesX = 20;
+  const nTilesY = 15;
+  const speed = 300; // time in ms to move 1 tile
+
   var board = document.getElementById("board")
   var contextFor2D = board.getContext("2d")
-  var nTilesX = 20;
-  var nTilesY = 15;
-  board.width = (nTilesX * tileSize())
-  board.height = (nTilesY * tileSize())
-  var snake = {x:getRandomInt(board.width), y:getRandomInt(board.height), dir:"right"} // xy wrt canvas origin
+  board.width = nTilesX * tileSize()
+  board.height = nTilesY * tileSize()
+  var snakePos = getRandomPosition(tileSize(), nTilesX, nTilesY)
+  var snake = {
+    x : snakePos.x, // xy of snake center wrt canvas origin (top left)
+    y : snakePos.y,
+    dir : "right"
+  }
+  var targetPos = getRandomPosition(tileSize(), nTilesX, nTilesY)
+  var target = {
+    x : targetPos.x,
+    y : targetPos.y
+  }
   var prevTime = performance.now() // ms since window loaded.
 
   document.addEventListener("keydown", changeDirection)
 
   window.requestAnimationFrame(updateBoard)
   function updateBoard(timeStamp){
-    moveSnake(timeStamp)
+    if (timeStamp - prevTime > speed) {
+      contextFor2D.clearRect(0, 0, board.width, board.height)
+      drawSnakeInNextPos()
+      drawTarget()
+      prevTime = timeStamp
+    }
+    
     if (outOfFrame()) {
       window.alert("You lost")
     } else {
@@ -27,46 +43,40 @@ window.onload = function(){
     switch (key) {
       case "ArrowUp" :
         snake.dir = "up"
-        break;
+        break
       case "ArrowDown" :
         snake.dir = "down"
-        break;
+        break
       case "ArrowRight" :
         snake.dir = "right"
-        break;
+        break
       case "ArrowLeft" :
         snake.dir = "left"
-        break;
+        break
     }
   }
 
-  function moveSnake(timeStamp){
-    console.log("Prev: " + prevTime)
-    console.log("Time: " + timeStamp)
-    if (timeStamp - prevTime > 500) {
-      contextFor2D.clearRect(0, 0, board.width, board.height)
-      switch (snake.dir) {
+  function drawSnakeInNextPos() {
+    switch (snake.dir) {
         case "up" :
           snake.y = snake.y - tileSize()
-          break;
+          break
         case "down" :
           snake.y = snake.y + tileSize()
-          break;
+          break
         case "right" :
           snake.x = snake.x + tileSize()
-          break;
+          break
         case "left" :
           snake.x = snake.x - tileSize()
-          break;
-
-      }
-      console.log("update position")
-
-      contextFor2D.beginPath()
-      contextFor2D.arc(snake.x, snake.y, (tileSize() / 2) - 1, 0, 2 * Math.PI)
-      contextFor2D.stroke()
-      prevTime = timeStamp
+          break
     }
+    console.log("update position")
+    drawCircle(snake.x, snake.y, tileSize() / 2 - 1)
+  }
+
+  function drawTarget() {
+    drawCircle(target.x, target.y, tileSize() / 2 - 3)
   }
 
   function tileSize() { // must be even
@@ -77,10 +87,24 @@ window.onload = function(){
     return Math.floor(Math.random() * max);
   }
 
+  function getRandomPosition(tileSize, nTilesX, nTilesY) {
+    var pos = {
+      x : getRandomInt(nTilesX - 1) * tileSize - (tileSize / 2),
+      y : getRandomInt(nTilesY - 1) * tileSize - (tileSize / 2)
+    }
+    return pos
+  }
+
   function outOfFrame() {
-    return snake.x + tileSize()/2 > board.width ||
+    return snake.x + tileSize() / 2 > board.width ||
     snake.x < 0 ||
-    snake.y + tileSize()/2 > board.height ||
+    snake.y + tileSize() / 2 > board.height ||
     snake.y < 0
+  }
+
+  function drawCircle(x, y, radius) {
+    contextFor2D.beginPath()
+    contextFor2D.arc(x, y, radius, 0, 2 * Math.PI)
+    contextFor2D.stroke()
   }
 };
